20fa14abc36b8acea5ba25fb1d31a960933c3cfb
dwikania <dwikania50@gmail.com>
2025-05-29 08:21:12 +0800

    Delete web_ui directory
    Signed-off-by: dwikania <dwikania50@gmail.com>

--- web_ui/bot_control.js
+++
@@ -1,8 +0,0 @@
-function startBots() {
-    document.getElementById('status').innerText = 'Bot dimulai...';
-    // TODO: implement API call ke backend untuk mulai bot
-}
-function stopBots() {
-    document.getElementById('status').innerText = 'Bot dihentikan...';
-    // TODO: implement API call ke backend untuk hentikan bot
-}
